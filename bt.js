1efbf15773c2be46880c2a94ef716d755e626e22
vinnyciusf5 <vinnyciusf5@hotmail.com>
2022-01-29 10:42:46 -0300

    main

--- bt.js
+++ bt.js
@@ -1,3 +1,11 @@
 function ig(){
-    window.document.getElementById('btig')
+    window.open("https://www.instagram.com/biiancamk/")
+}
+
+function wz(){
+    window.open('https://api.whatsapp.com/send?phone=5573999891835')
+}
+
+function promo(){
+    window.location.href='promocao.html'
 }
